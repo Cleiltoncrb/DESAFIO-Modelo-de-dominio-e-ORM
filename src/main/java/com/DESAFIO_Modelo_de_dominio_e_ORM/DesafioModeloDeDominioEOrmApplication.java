9998d9e2ad29a1c15a1e0dd7b42100b832747ab2
Cleilton - CRB <cleilton.oi18@gmail.br>
2025-03-25 19:08:32 -0300

    Start - Ok

---
+++ src/main/java/com/DESAFIO_Modelo_de_dominio_e_ORM/DesafioModeloDeDominioEOrmApplication.java
@@ -0,0 +1,13 @@
+package com.DESAFIO_Modelo_de_dominio_e_ORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioModeloDeDominioEOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioModeloDeDominioEOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DESAFIO_Modelo_de_dominio_e_ORM/DesafioModeloDeDominioEOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DESAFIO_Modelo_de_dominio_e_ORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioModeloDeDominioEOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
